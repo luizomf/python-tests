0f57cb624aa8eb30b8256ecdb378fc9a9e1f9898
Luiz Otavio <luizomf@gmail.com>
2020-01-25 17:00:45 -0300

    Doctests

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,38 @@
 def soma(x, y):
+    """Soma x e y
+
+    >>> soma(10, 20)
+    30
+
+    >>> soma(-10, 20)
+    10
+
+    >>> soma('10', 20)
+    Traceback (most recent call last):
+    ...
+    AssertionError: x precisa ser int ou float
+    """
     assert isinstance(x, (int, float)), 'x precisa ser int ou float'
     assert isinstance(y, (int, float)), 'y precisa ser int ou float'
     return x + y
+
+
+def subtrai(x, y):
+    """Subtrai x e y
+
+    >>> subtrai(10, 5)
+    5
+
+    >>> subtrai('10', 5)
+    Traceback (most recent call last):
+    ...
+    AssertionError: x precisa ser int ou float
+    """
+    assert isinstance(x, (int, float)), 'x precisa ser int ou float'
+    assert isinstance(y, (int, float)), 'y precisa ser int ou float'
+    return x - y
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(verbose=True)
