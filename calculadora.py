136f04266213290838bda2f6fc8394b6bbc02bb9
Luiz Otavio <luizomf@gmail.com>
2020-01-25 15:31:17 -0300

    Assercoes

--- calculadora.py
+++ calculadora.py
@@ -1,2 +1,4 @@
-def soma(x, y, z):
+def soma(x, y):
+    assert isinstance(x, (int, float)), 'x precisa ser int ou float'
+    assert isinstance(y, (int, float)), 'y precisa ser int ou float'
     return x + y
--- main.py
+++ main.py
@@ -5,3 +5,10 @@ from calculadora import soma
 # print(soma(10, 20))
 # print(soma(-10, 20))
 # print(soma(1.5, 2.5))
+
+try:
+    print(soma('15', 15))
+except AssertionError as e:
+    print(f'Conta inválida: {e}')
+
+print('Conta', soma(25, 25))
