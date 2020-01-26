39129a6600a2926ef5907035df499d8792376b7d
Luiz Otavio <luizomf@gmail.com>
2020-01-26 01:15:44 -0300

    Unittest 1

---
+++ test_calculadora.py
@@ -0,0 +1,35 @@
+import unittest
+from calculadora import soma
+
+
+class TestCalculadora(unittest.TestCase):
+    def test_soma_5_e_5_deve_retornar_10(self):
+        self.assertEqual(soma(5, 5), 10)
+
+    def test_soma_5_negativo_e_5_deve_retornar_0(self):
+        self.assertEqual(soma(-5, 5), 0)
+
+    def test_soma_varias_entradas(self):
+        x_y_saidas = (
+            (10, 10, 20),
+            (5, 5, 10),
+            (1.5, 1.5, 3.0),
+            (-5, 5, 0),
+            (100, 100, 200),
+        )
+
+        for x_y_saida in x_y_saidas:
+            with self.subTest(x_y_saida=x_y_saida):
+                x, y, saida = x_y_saida
+                self.assertEqual(soma(x, y), saida)
+
+    def test_soma_x_nao_e_int_ou_float_deve_retornar_assertionerror(self):
+        with self.assertRaises(AssertionError):
+            soma('11', 0)
+
+    def test_soma_y_nao_e_int_ou_float_deve_retornar_assertionerror(self):
+        with self.assertRaises(AssertionError):
+            soma(11, '0')
+
+
+unittest.main(verbosity=2)
